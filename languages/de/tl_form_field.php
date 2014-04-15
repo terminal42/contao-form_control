a72f443d117eca5e4a6e90af260e2139dc262bf5
Yanick Witschi <yanick.witschi@terminal42.ch>
2014-04-15 14:17:32 +0200

    Added german language

---
+++ languages/de/tl_form_field.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * form_control extension for Contao Open Source CMS
+ *
+ * @copyright  Copyright (c) 2008-2014, terminal42 gmbh
+ * @author     terminal42 gmbh <info@terminal42.ch>
+ * @license    http://opensource.org/licenses/lgpl-3.0.html LGPL
+ * @link       http://github.com/terminal42/contao-form_control
+ */
+
+
+/**
+ * Fields
+ */
+$GLOBALS['TL_LANG']['tl_form_field']['formcontrol_template'] = array('Form Control Template', 'Wählen Sie hier das Form Control Template.');
+
+
+/**
+ * Legends
+ */
+$GLOBALS['TL_LANG']['tl_form_field']['formcontrol_legend'] = 'Form Control Einstellungen';
